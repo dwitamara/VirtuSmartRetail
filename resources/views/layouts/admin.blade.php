505b7b8019f83f46aec8e46e446b4e4018a1af00
Fitranda Ramadhana <68302339+Fitranda@users.noreply.github.com>
2024-11-16 10:56:18 +0700

    logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -242,6 +242,24 @@
                 </ul>
 
             </nav>
+            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Anda yakin untuk keluar</h5>
+                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">Jika yakin keluar maka klik logout.</div>
+                            <div class="modal-footer">
+                                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                <a class="btn btn-primary" href="{{ route('login') }}">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @yield('content')
         </div>
     </div>
